28a548c0d8a6d6331d8f827f1158692220b7553b
prvn_kmr <parveen.kumar@inficaretech.com>
2017-07-04 17:55:43 +0530

    commit -m "bug fix"

--- js/TableToJson.js
+++ js/TableToJson.js
@@ -1,26 +1,27 @@
-var makeJsonFromTable=function(tableID)
-{
-    var tbl=$('#'+tableID)
-	var tblhead=$(tbl).find('thead')
-	var tblbody=$(tbl).find('tbody')
-	var tblbodyCount=$(tbl).find('tbody>tr').length;
-	var header=[];
-	var JObjectArray=[];
-	$.each($(tblhead).find('tr>th'),function(i,j){
+var makeJsonFromTable = function(tableID) {
+    var tbl = $('#' + tableID)
+    if ($(tbl).prop("tagName") == "TABLE") {
+        alert("Somthing is going worng !");
+        return false;
+    }
+    var tblhead = $(tbl).find('thead')
+    var tblbody = $(tbl).find('tbody')
+    var tblbodyCount = $(tbl).find('tbody>tr').length;
+    var header = [];
+    var JObjectArray = [];
+    $.each($(tblhead).find('tr>th'), function(i, j) {
         header.push($(j).text())
-	})
-	$.each($(tblbody).find('tr'),function(key,value)
-	{
-	   var jObject={};
-	   for(var x=0 ; x < header.length;x++)
-	   {
-		 jObject[header[x]]=$(this).find('td').eq(x).text()
-	   }
-	   JObjectArray.push(jObject)
-	
-	});
-	var jsonObject={};
-	jsonObject["count"]=tblbodyCount
-	jsonObject["value"]=JObjectArray;
-	return jsonObject;
+    })
+    $.each($(tblbody).find('tr'), function(key, value) {
+        var jObject = {};
+        for (var x = 0; x < header.length; x++) {
+            jObject[header[x]] = $(this).find('td').eq(x).text()
+        }
+        JObjectArray.push(jObject)
+
+    });
+    var jsonObject = {};
+    jsonObject["count"] = tblbodyCount
+    jsonObject["value"] = JObjectArray;
+    return jsonObject;
 }
